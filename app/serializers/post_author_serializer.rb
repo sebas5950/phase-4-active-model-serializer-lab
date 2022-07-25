ece3a3f7b3f79a21d484d69a6525ba5477f377c8
Alex Guanipatin <sebas5950@gmail.com>
2022-07-24 23:17:30 -0400

    commit tree f5e7e8e54e2200f48bc3404008fd3a9131f4d078

---
+++ app/serializers/post_author_serializer.rb
@@ -0,0 +1,9 @@
+class PostAuthorSerializer < ActiveModel::Serializer
+  attributes :title, :short_content
+
+  has_many :tags
+
+  def short_content
+    "#{self.object.content[0..39]}..."
+  end
+end
